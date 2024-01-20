a2ea9a0345d54a1aa918dcf39e42a2df164988ca
John Jaca <johnangelo.jaca@letran.edu.ph>
2024-01-20 17:14:54 +0800

    haha sige

---
+++ src/app/ci/ci.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ci',
+  standalone: true,
+  imports: [],
+  templateUrl: './ci.component.html',
+  styleUrl: '../../../node_modules/bootstrap/dist/css/bootstrap.css'
+})
+export class CiComponent {
+
+}
---
+++ src/app/pi/pi.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pi',
+  standalone: true,
+  imports: [],
+  templateUrl: './pi.component.html',
+  styleUrl: '../../../node_modules/bootstrap/dist/css/bootstrap.css'
+})
+export class PiComponent {
+
+}
