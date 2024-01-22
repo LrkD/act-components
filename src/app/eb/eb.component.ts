c38cf67fb13bf13fa63155a9fdbee03df61387cb
LrkD <karlcedrick.deluda@letran.edu.ph>
2024-01-22 21:25:05 +0800

    ngayonn to ginawa

---
+++ src/app/eb/eb.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eb',
+  standalone: true,
+  imports: [],
+  templateUrl: './eb.component.html',
+  styleUrl: '../../../node_modules/bootstrap/dist/css/bootstrap.css'
+})
+export class EbComponent {
+
+}
---
+++ src/app/we/we.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-we',
+  standalone: true,
+  imports: [],
+  templateUrl: './we.component.html',
+  styleUrl: '../../../node_modules/bootstrap/dist/css/bootstrap.css'
+})
+export class WeComponent {
+
+}
